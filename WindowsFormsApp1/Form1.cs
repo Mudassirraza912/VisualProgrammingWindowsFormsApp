d375e3c0225571ac3e6eb1eb590a4de2f769440a
Mudassir Raza <mraza@esparkconsultants.com>
2023-03-09 13:49:11 +0500

    basic form

---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            StreamWriter sw = new StreamWriter(@"D:\MyData\File_"+Services.Guid()+".txt");
+            sw.WriteLine(textBox1.Text);
+            sw.Close();
+        }
+    }
+}
---
+++ WindowsFormsApp1/Services.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp1
+{
+    public class Services
+    {
+        public static string Guid()
+        {
+            int year = DateTime.Now.Year;
+            int month = DateTime.Now.Month;
+            int day = DateTime.Now.Day;
+            int hours = DateTime.Now.Hour;
+            int mins = DateTime.Now.Minute;
+            int secs = DateTime.Now.Second;
+
+            return year.ToString() + month.ToString() + day.ToString() + hours.ToString() + mins.ToString() + secs.ToString();
+
+        }
+    }
+}
